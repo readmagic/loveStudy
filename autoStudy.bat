e48cb1522a27fbde686605ca8e24443f7e9254aa
frandy <303734023@qq.com>
2021-07-29 15:27:21 +0800

    代码初始化

--- autoStudy.bat
+++ autoStudy.bat
@@ -2,6 +2,7 @@
 chcp 65001
 REM 模拟器路径
 start C:\"Program Files (x86)"\MuMu\emulator\nemu\EmulatorShell\NemuPlayer.exe
-adb kill-server
-adb connect 127.0.0.1:7555
+
+
+interface portproxy add v4tov4 listenport=5555 listenaddress=172.16.24.80 connectport=7555 connectaddress=127.0.0.1
 pause

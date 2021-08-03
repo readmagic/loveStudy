e95112b945ec34f1522ffcda758f15aa21289948
frandy <303734023@qq.com>
2021-08-03 23:56:42 +0800

    代码初始化

--- study.py
+++ study.py
@@ -2,26 +2,30 @@
 import sys
 
 from uiautomator import device as driver
+import qrcode
 import numpy as np
 import time
 import os
 
-Height=1280
-Width=720
-all_of_list=[]
+Height = 1280
+Width = 720
+EXE = 'D:\\XuexiXuexi-master\\xxqg\\study.exe'
+TOKEN='D:\\XuexiXuexi-master\\xxqg\\token.txt'
+all_of_list = []
 if os.path.isfile("db.npy"):
-    all_of_list = np.load ("db.npy").tolist()
+    all_of_list = np.load("db.npy").tolist()
 
-all_of_list2=[]
+all_of_list2 = []
 if os.path.isfile("db2.npy"):
-    all_of_list2 = np.load ("db2.npy").tolist()
+    all_of_list2 = np.load("db2.npy").tolist()
+
 
 def login(pwd):
     try:
         driver(text="登录").click()
-        driver.click(0.5*Width, 0.42*Height)
+        driver.click(0.5 * Width, 0.42 * Height)
         time.sleep(1)
-        os.system("adb shell am broadcast -a ADB_INPUT_TEXT --es msg '%s'"%pwd)
+        os.system("adb shell am broadcast -a ADB_INPUT_TEXT --es msg '%s'" % pwd)
         time.sleep(1)
         driver(text="登录").click()
         time.sleep(2)
@@ -31,59 +35,87 @@ def login(pwd):
         print('正在登录')
 
 
-def autoJob(tv,sleep_time,sum=6,click=True):
-    count_click=0
-    count=0
-    drag_str='adb shell input swipe '+str(Width*0.5)+' '+str(Height*0.88)+' '+str(Width*0.5)+' '+str(Height*0.3)
+def send_email():
+    from email.header import Header
+    from email.mime.multipart import MIMEMultipart
+    from email.mime.image import MIMEImage
+    msg = MIMEMultipart()
+    msg['Subject'] = Header('学习强国', 'utf-8').encode()
+    msg['from'] = '249113537@163.com'
+    msg['to'] = '303734023@qq.com'
+
+    file = open("a.png", "rb")
+    img_data = file.read()
+    file.close()
+    img = MIMEImage(img_data)
+    img.add_header('Content-Disposition', 'attachment', filename="a.png")
+    msg.attach(img)
+
+    user = '249113537@163.com'
+    password = 'TIXWHDJRIYGEBLXY'
+    smtp_server = 'smtp.163.com'
+    to_addr = '303734023@qq.com'
+    import smtplib
+    server = smtplib.SMTP(smtp_server, 25)  # Smtp协议默认窗口是25
+    server.login(user, password)
+    server.sendmail(user, to_addr, msg.as_string())
+    server.quit()
+
+
+def autoJob(tv, sleep_time, sum=6, click=True):
+    count_click = 0
+    count = 0
+    drag_str = 'adb shell input swipe ' + str(Width * 0.5) + ' ' + str(Height * 0.88) + ' ' + str(
+        Width * 0.5) + ' ' + str(Height * 0.3)
     for _ in range(100):
-        text_lists=driver(className='android.widget.TextView')
+        text_lists = driver(className='android.widget.TextView')
         try:
             for i in range(len(text_lists)):
-                txt=text_lists[i].text
-                if len(txt)>11 and txt not in all_of_list and count<sum:
-                    driver(text=txt,className='android.widget.TextView').click()
-                    #分享，收藏，评论
-                    if click and count_click<2:
-                        #分享
+                txt = text_lists[i].text
+                if len(txt) > 11 and txt not in all_of_list and count < sum:
+                    driver(text=txt, className='android.widget.TextView').click()
+                    # 分享，收藏，评论
+                    if click and count_click < 2:
+                        # 分享
                         time.sleep(4)
-                        driver.click(0.94*Width, 0.975*Height)
+                        driver.click(0.94 * Width, 0.975 * Height)
                         time.sleep(2)
                         driver(text="分享到学习强国").click()
                         time.sleep(2)
                         driver.press.back()
-                        #收藏
-                        driver.click(0.84*Width, 0.975*Height)
-                        #评论
+                        # 收藏
+                        driver.click(0.84 * Width, 0.975 * Height)
+                        # 评论
                         time.sleep(1)
                         driver(text="欢迎发表你的观点").click()
                         time.sleep(2)
                         os.system("adb shell am broadcast -a ADB_INPUT_TEXT --es msg '中国加油！全力支持'")
-                        os.system("adb shell input keyevent 66")#不知道为什么输入一个回车，点击发布才有反应
+                        os.system("adb shell input keyevent 66")  # 不知道为什么输入一个回车，点击发布才有反应
                         time.sleep(2)
                         driver(text="发布").click()
                         time.sleep(1)
-                        driver.click(0.94*Width, 0.864*Height)
-                        count_click=count_click+1
+                        driver.click(0.94 * Width, 0.864 * Height)
+                        count_click = count_click + 1
 
-                        #取消收藏
+                        # 取消收藏
                         time.sleep(3)
-                        driver.click(0.84*Width, 0.975*Height)
+                        driver.click(0.84 * Width, 0.975 * Height)
                         time.sleep(1)
-                        driver.click(0.84*Width, 0.975*Height)
-                        #删除发布的评论
+                        driver.click(0.84 * Width, 0.975 * Height)
+                        # 删除发布的评论
                         time.sleep(2)
                         driver(text="删除").click()
                         time.sleep(2)
                         driver(text="确认").click()
 
-                    count=count+1
+                    count = count + 1
                     all_of_list.append(txt)
-                    print("正在"+tv+"...",txt)
+                    print("正在" + tv + "...", txt)
                     time.sleep(sleep_time)
                     driver.press.back()
         except BaseException:
             print("抛出异常，程序继续执行...")
-        if count >=sum:
+        if count >= sum:
             break
         os.system(drag_str)
 
@@ -97,37 +129,38 @@ def watch_local():
     print("本地频道结束")
     driver.press.back()
 
-#阅读文章,阅读6个文章，每个文章停留130秒
+
+# 阅读文章,阅读6个文章，每个文章停留130秒
 def read_articles():
     time.sleep(2)
-    #切换到要闻界面
-    driver(text='综合').click()
-    autoJob(tv="阅读文章",sleep_time=135)
+    # 切换到要闻界面
+    driver(text='新思想').click()
+    autoJob(tv="阅读文章",sum=1, sleep_time=130)
 
 
-#观看视频,每个视频观看20秒，以及17分钟新闻联盟
+# 观看视频,每个视频观看20秒，以及17分钟新闻联盟
 def watch_video():
     time.sleep(2)
-    #切换到电视台页面
+    # 切换到电视台页面
     driver(resourceId="cn.xuexi.android:id/home_bottom_tab_button_contact").click()
     driver(text="联播频道").click()
-    autoJob(tv="观看视频",sleep_time=25,click=False)
+    autoJob(tv="观看视频", sleep_time=25, click=False)
     driver(text="联播频道").click()
 
-    news=None
+    news = None
     for v in driver(className='android.widget.TextView'):
         if "《新闻联播》" in v.text:
-            news=v.text
+            news = v.text
             break
     driver(text=news).click()
 
-    #100天后删除最早一天的记录
-    text_list=np.array (all_of_list)
+    # 100天后删除最早一天的记录
+    text_list = np.array(all_of_list)
 
-    if len(text_list)>2500:
+    if len(text_list) > 2500:
         text_list = text_list[25:]
-    #存储已看视频和文章
-    np.save ('db.npy',text_list)
+    # 存储已看视频和文章
+    np.save('db.npy', text_list)
 
     print("正在观看新闻联播...")
     time.sleep(1050)
@@ -135,21 +168,109 @@ def watch_video():
     print("观看视频结束.")
 
 
+def end_program(pro_name):
+    os.system('%s%s' % ("taskkill /F /IM ", pro_name))
+
+
+import requests, time, logging
+
+
+def retry(times: int, interval: int):
+    def wrapper(func):
+        def inner(*args, **kwarys):
+            v = None
+            for _ in range(times):
+                try:
+                    v = func(*args, **kwarys)
+                except Exception as err:
+                    logging.error(str(err))
+                    time.sleep(interval)
+                    continue
+                break
+            return v
+
+        return inner
+
+    return wrapper
+
+
+session = requests.Session()
+
+
+@retry(times=100, interval=1)
+def getQrcode():
+    qr = session.get('https://login.xuexi.cn/user/qrcode/generate').json()['result']
+    url = "https://login.xuexi.cn/login/qrcommit?showmenu=false&code={qr}&appId=dingoankubyrfkttorhpou".format(qr=qr)
+    img = qrcode.make(url)
+    img.save("a.png")
+    return qr
+
+
+@retry(times=100, interval=1)
+def getTmpCode(qrcode: str):
+    url = 'https://login.xuexi.cn/login/login_with_qr'
+    res = session.post(url, data={
+        "qrCode": qrcode,
+        "goto": "https://oa.xuexi.cn",
+        "pdmToken": ""
+    }).json()
+    if not res['success']:
+        raise Exception(str(res))
+    data = res['data']
+    key = "loginTmpCode="
+    tmpCode = data[data.index(key) + len(key):]
+    return tmpCode
+
+
+@retry(times=100, interval=1)
+def getCookies(tmpCode: str):
+    url = 'https://pc-api.xuexi.cn/login/secure_check?code={tmpCode}&state=06d81817e84a430fRn3K4e9Temmx7XsXcJVvnBCy7b8DrBR0vkAIQQtA0wCDp0owW5W1o9XkX7aUUZ16'.format(
+        tmpCode=tmpCode)
+    res = session.get(url)
+    cookies = res.cookies
+    res = res.json()
+    if not res['success']:
+        raise Exception(str(res))
+    return [{'name': c.name, 'value': c.value, 'domain': c.domain, 'path': c.path} for c in cookies]
+
+
+@retry(times=100000, interval=1)
+def web_login():
+    qr = getQrcode()
+    send_email()
+    tmpCode = getTmpCode(qr)
+    cookies = getCookies(tmpCode)
+    return cookies
+
+
 if __name__ == '__main__':
-    password = sys.argv[1]
-    #自动打开学习强国
-    os.system('adb shell am start cn.xuexi.android/com.alibaba.android.rimet.biz.SplashActivity')
-    time.sleep(1)
-    #屏幕高度
-    Height=driver.info['displayHeight']
-    Width=driver.info['displayWidth']
-    #切换adb输入法
-    os.system('adb shell ime set com.android.adbkeyboard/.AdbIME')
-    login(password)
-    watch_local()
-    read_articles()
-    watch_video()
-    #切换回搜狗输入法
-    #os.system('adb shell ime set com.sohu.inputmethod.sogou.meizu/com.sohu.inputmethod.sogou.SogouIME')
-    #熄灭屏幕
-    os.system('adb shell input keyevent 26')
+    try:
+        password = sys.argv[1]
+        # #自动打开学习强国
+        os.system('adb shell am start cn.xuexi.android/com.alibaba.android.rimet.biz.SplashActivity')
+        time.sleep(5)
+        # #屏幕高度
+        Height = driver.info['displayHeight']
+        Width = driver.info['displayWidth']
+        # 切换adb输入法
+        os.system('adb shell ime set com.android.adbkeyboard/.AdbIME')
+        login(password)
+        watch_local()
+        read_articles()
+        # watch_video()
+        # 关闭app
+        os.system('adb shell am force-stop cn.xuexi.android')
+    except Exception:
+        print("执行手机端app报错")
+
+    cookies = web_login()
+    for cookie in cookies:
+        if cookie['name']=='token':
+            token = cookie['value']
+            fo = open(TOKEN, "w")
+            fo.write("token="+token+"; domain=.xuexi.cn; path=/")
+            fo.close()
+            break
+    os.system('start '+EXE)
+    time.sleep(500)
+    end_program('study.exe')
